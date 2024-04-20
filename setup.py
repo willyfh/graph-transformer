e9b176800a999f53a1f73c417ada2c630d80cfaa
Willy Fitra Hendria <willyfitrahendria@gmail.com>
2024-04-20 20:06:01 +0900

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name='graph-transformer',
     description='This is the implementation of Graph Transformer (https://www.ijcai.org/proceedings/2021/0214.pdf)',
     packages=find_packages(),
-    version='0.2.0',
+    version='0.2.1',
     author='Willy Fitra Hendria',
     author_email = 'willyfitrahendria@gmail.com',
     url = 'https://github.com/willyfh/graph-transformer',
